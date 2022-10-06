5adabf871acff13e51e9e3bd3bfe7b04b7074394
Herman <herman@q42.nl>
2022-10-06 11:32:04 +0200

    feat: url lib

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/hermanbanken/cloudrun
+
+go 1.18
+
+require (
+	cloud.google.com/go/compute v1.10.0
+	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ url/url.go
@@ -0,0 +1,75 @@
+package url
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/compute/metadata"
+	"golang.org/x/oauth2/google"
+)
+
+var ErrNotCloudRun = errors.New("Not running on CloudRun")
+
+// CloudRunUrl returns the public url of the CloudRun instance
+// https://medium.com/google-cloud/3-solutions-to-mitigate-the-cold-starts-on-cloud-run-8c60f0ae7894
+func CloudRunUrl() (*url.URL, error) {
+	if service, has := os.LookupEnv("K_SERVICE"); has {
+		projectNr, region, err := ProjectAndRegion()
+		if err != nil {
+			return nil, err
+		}
+		uri, err := getCloudRunUrl(region, projectNr, service)
+		if err != nil {
+			return nil, err
+		}
+		return url.Parse(uri)
+	}
+	return nil, ErrNotCloudRun
+}
+
+func ProjectAndRegion() (prNb string, region string, err error) {
+	resp, err := metadata.Get("/instance/region")
+	if err != nil {
+		return
+	}
+	// response pattern is projects/<projectNumber>/regions/<region>
+	r := strings.Split(resp, "/")
+	prNb = r[1]
+	region = r[3]
+	return
+}
+
+func getCloudRunUrl(region string, projectNumber string, service string) (url string, err error) {
+	ctx := context.Background()
+	client, err := google.DefaultClient(ctx)
+
+	cloudRunApi := fmt.Sprintf("https://%s-run.googleapis.com/apis/serving.knative.dev/v1/namespaces/%s/services/%s", region, projectNumber, service)
+	resp, err := client.Get(cloudRunApi)
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+
+	}
+	cloudRunResp := &cloudRunAPIUrlOnly{}
+	json.Unmarshal(body, cloudRunResp)
+	url = cloudRunResp.Status.URL
+	return
+}
+
+// Minimal type to get only the interesting part in the answer
+type cloudRunAPIUrlOnly struct {
+	Status struct {
+		URL string `json:"url"`
+	} `json:"status"`
+}
